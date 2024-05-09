bb1f2708dfa9efaf61014dcfe43fdfb1fa342add
Garr17 <131855934+Garr17@users.noreply.github.com>
2024-05-09 17:17:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to the ECR !
+          Welcome to the CICD Pipeline and  ECR !
         </a>
       </header>
     </div>
