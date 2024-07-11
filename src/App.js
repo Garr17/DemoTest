70830890245a778492b1ab18f14f9dd98ea53243
Garr17 <131855934+Garr17@users.noreply.github.com>
2024-07-11 13:44:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome!
+          Welcome to APIIT!
         </a>
       </header>
     </div>
