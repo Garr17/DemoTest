e31861d398f418e37ad9d7a9e7cc8f4f5c53cc55
Garr17 <131855934+Garr17@users.noreply.github.com>
2025-07-09 11:07:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to Team virtusa and TTBD 
+          Welcome to  Virtusa
         </a>
       </header>
     </div>
